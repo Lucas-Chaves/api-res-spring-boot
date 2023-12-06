067b20ce11de441112cc365976883a283392427c
Lucas Chaves <lucas.limachaves@outlook.com>
2023-12-06 14:07:08 -0300

    fix more code analysis

--- src/test/java/com/lucas/demorparkapi/DemorParkApiApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.lucas.demorparkapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemorParkApiApplicationTests {
-
-}
